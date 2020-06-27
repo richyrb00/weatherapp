7cd328948107dae9eade3be8c9a23ddf56c5685e
Richard Bilton <me@richardbilton.tv>
2020-06-27 21:26:22 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+    weather: [],
+  };
+
+  componentDidMount() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Knutsford,uk&appid=${process.env.REACT_APP_WEATHERAPI}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ data: data });
+        this.setState({ weather: data.weather[0] });
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    const { data = undefined } = this.state;
+    const { weather = undefined } = this.state;
+    console.log("env", process.env.REACT_APP_WEATHERAPI);
+
+    if (data && weather) {
+      const { main, clouds, coord, name, sys, wind } = this.state.data;
+      console.log("data", this.state.data);
+      console.log("weather", this.state.weather);
+      return (
+        <div className="App" data-api={[process.env.REACT_APP_WEATHERAPI]}>
+          <header className="App-header">
+            <h1>{name}</h1>
+
+            <p>{this.state.weather.description}</p>
+
+            {this.state.weather.icon ? (
+              <img
+                src={`/images/${this.state.weather.icon}.png`}
+                alt={this.state.weather.description}
+                className="weather-icon"
+              />
+            ) : null}
+          </header>
+          <main></main>
+        </div>
+      );
+    } else {
+      return <div>Loading</div>;
+    }
+  }
 }
 
 export default App;
