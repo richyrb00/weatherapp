09cebb16ffbf5443adf7993a77b7dfe49bc96e7f
Richard Bilton <me@richardbilton.tv>
2020-06-27 21:32:01 +0100

    Updates

---
+++ precache-manifest.4404f870f317d0800a552f2e58310c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3fd43821022df2b77e7f13cd2955c0",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "67610ea30c3690348846",
+    "url": "/weatherapp/static/css/main.102c6fc8.chunk.css"
+  },
+  {
+    "revision": "1487d72c1e7ce49c65c0",
+    "url": "/weatherapp/static/js/2.b5ea3870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.b5ea3870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67610ea30c3690348846",
+    "url": "/weatherapp/static/js/main.faf4e132.chunk.js"
+  },
+  {
+    "revision": "ff907cfbbb256dae02f8",
+    "url": "/weatherapp/static/js/runtime-main.f16cac52.js"
+  }
+]);
