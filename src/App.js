ba68b8c6df62de69b83a274544008959b96bdd3b
Richard Bilton <me@richardbilton.tv>
2020-06-29 00:27:49 +0100

    updated chart colors

--- src/App.js
+++ src/App.js
@@ -123,18 +123,18 @@ class App extends Component {
           label: "Temperature",
           fill: false,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
+          backgroundColor: "#9b2c2c",
+          borderColor: "#9b2c2c",
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
+          pointBorderColor: "#9b2c2c",
           pointBackgroundColor: "#fff",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBackgroundColor: "#c53030",
+          pointHoverBorderColor: "#c53030",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
@@ -192,7 +192,7 @@ class App extends Component {
           </div>
         </header>
         <main className="flex items-center justify-center">
-          <div className="w-fill p-6 md:w-3/6 lg:w-3/6">
+          <div className="w-fill p-6 md:w-3/6 lg:w-3/6 relative">
             <Line data={temp_chart} />
           </div>
         </main>
