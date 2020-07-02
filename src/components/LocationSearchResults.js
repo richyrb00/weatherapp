ec9783523e48183147198e358ba27e532534f207
Richard Bilton <me@richardbilton.tv>
2020-07-02 22:48:44 +0100

    removed exact from route

--- src/components/LocationSearchResults.js
+++ src/components/LocationSearchResults.js
@@ -33,6 +33,7 @@ export default class LocationSearchResults extends Component {
             <a
               href={`${process.env.PUBLIC_URL}/location/${location.coord.lat}/${location.coord.lon}`}
               className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none align-middle"
+              data-public={process.env.PUBLIC_URL}
             >
               View
             </a>
